5e71cb53ce50ceb62a37bd6260bb9a97b1cd4d9d
ppfoerts <pf23.ft@googlemail.com>
2021-12-18 21:39:50 +0100

    day 18, didn't solve couldn't figure out exploding
    I went to the subreddit and found a binary tree example. Others used a string. Neither solution was something I came up with.

---
+++ day18/day18.py
@@ -0,0 +1,28 @@
+# Day 18 Snailfish
+#!/usr/bin/python
+
+import math
+
+
+# the pair's left value is added to the first regular number to the left of the 
+# exploding pair (if any), and the pair's right value is added to the first regular 
+# number to the right of the exploding pair (if any)
+def explodePair():
+    x = 1
+
+def splitPair(value):
+    x = value // 2
+    y = math.ceil(value / 2)
+    return [x,y]
+
+def reduceSnailFishNumber():
+    # If any pair is nested inside four pairs, leftmost such pair explodes
+    if():
+        y = 1
+    # If any regular number is 10 or greater, the leftmost such regular number splits
+    elif():
+        x = 1
+    else:
+        return
+
+print(splitPair(5))
